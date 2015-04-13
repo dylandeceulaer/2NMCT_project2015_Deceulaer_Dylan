2ae2aa3a664832f473e18b70e00c74b66d7e0033
dylandeceulaer <dylan.deceulaer@student.howest.be>
2015-04-13 15:35:13 +0200

    drawables en non commited files

---
+++ app/src/main/java/be/howest/dylandeceulaer/places/Data.java
@@ -0,0 +1,171 @@
+package be.howest.dylandeceulaer.places;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Dylan on 12/04/2015.
+ */
+public class Data {
+
+    private SQLiteDatabase database;
+    private DatabaseHelper dbHelper;
+    private String[] allColumns = { DatabaseHelper.colMarkerInfoID,
+    DatabaseHelper.colTitle,
+    DatabaseHelper.colAddress,
+    DatabaseHelper.colPosLat,
+    DatabaseHelper.colPosLong,
+    DatabaseHelper.colMarkerIcon};
+
+    public enum MARKER{
+        HOME("huis",R.drawable.custom_marker_home,R.drawable.custom_marker_home_icon),
+        POI("Point of interest",R.drawable.custom_marker_poi,R.drawable.custom_marker_poi_icon);
+
+        private String naam;
+        private int marker;
+        private int markerIcon;
+
+        MARKER(String naam, int marker, int markerIcon){
+            this.naam = naam;
+            this.marker = marker;
+            this.markerIcon = markerIcon;
+        }
+
+        public String getNaam() {
+            return naam;
+        }
+
+        public int getMarker() {
+            return marker;
+        }
+
+        public int getMarkerIcon() {
+            return markerIcon;
+        }
+
+        public static MARKER getMarker(int id){
+            for(MARKER marker : MARKER.values()){
+                if(marker.getMarker() == id) return marker;
+            }
+            return MARKER.HOME;
+        }
+
+    }
+
+    public Data(Context context) {
+        dbHelper = new DatabaseHelper(context);
+
+    }
+
+    public void open() throws SQLException {
+        database = dbHelper.getWritableDatabase();
+        System.out.println("hallo");
+        if(database == null)System.out.println("hallo2");
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    public long addMarker(MarkerInfo markerInfo){
+        open();
+        ContentValues values = new ContentValues();
+        values.put(DatabaseHelper.colTitle,markerInfo.getTitel());
+        values.put(DatabaseHelper.colAddress, markerInfo.getAdres());
+        values.put(DatabaseHelper.colPosLat, markerInfo.getPositie().latitude);
+        values.put(DatabaseHelper.colPosLong, markerInfo.getPositie().longitude);
+        values.put(DatabaseHelper.colMarkerIcon, markerInfo.getMarkerData().getMarker());
+
+        if(database == null) System.out.println("database");
+
+
+        long a =  database.insert(DatabaseHelper.markerInfoTable, null, values);
+        close();
+        return a;
+    }
+
+    public List<MarkerInfo> getSavedMarkers(){
+        open();
+
+        List<MarkerInfo> lijst = new ArrayList<MarkerInfo>();
+
+        Cursor cursor = database.query(DatabaseHelper.markerInfoTable,allColumns,null,null,null,null,null);
+
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()){
+            lijst.add(cursorToMarkerInfo(cursor));
+            System.out.println(cursor.getString(1));
+
+            cursor.moveToNext();
+        }
+        close();
+        return lijst;
+    }
+    private MarkerInfo cursorToMarkerInfo(Cursor cursor){
+        long id = cursor.getLong(0);
+        String title = cursor.getString(1);
+        String address = cursor.getString(2);
+        LatLng pos = new LatLng(cursor.getDouble(3),cursor.getDouble(4));
+        MARKER marker = MARKER.getMarker(cursor.getInt(5));
+
+        MarkerInfo mi = new MarkerInfo(title,pos,address,marker);
+        mi.setId(id);
+
+        return mi;
+
+
+        //return null;
+    }
+    public MarkerInfo getMarkerByPosition(Marker marker){
+        open();
+
+        MarkerInfo res;
+        String whereClause = DatabaseHelper.colPosLat+" = ? AND "+DatabaseHelper.colPosLong+" = ?";
+        String [] whereArgs = new String [] {
+                ""+marker.getPosition().latitude,
+                ""+marker.getPosition().longitude
+        };
+
+        Cursor cursor = database.query(DatabaseHelper.markerInfoTable,allColumns,whereClause,whereArgs,null,null,null);
+
+        cursor.moveToFirst();
+        res = cursorToMarkerInfo(cursor);
+        close();
+
+        return res;
+    }
+
+    public void deleteMarker(MarkerInfo marker){
+        open();
+
+        database.delete(DatabaseHelper.markerInfoTable,DatabaseHelper.colMarkerInfoID+" = "+marker.getId(),null);
+        close();
+    }
+
+    public void updateMarker(MarkerInfo markerInfo){
+
+        open();
+
+        ContentValues values = new ContentValues();
+        values.put(DatabaseHelper.colTitle,markerInfo.getTitel());
+        values.put(DatabaseHelper.colAddress, markerInfo.getAdres());
+        values.put(DatabaseHelper.colPosLat, markerInfo.getPositie().latitude);
+        values.put(DatabaseHelper.colPosLong, markerInfo.getPositie().longitude);
+        values.put(DatabaseHelper.colMarkerIcon, markerInfo.getMarkerData().getMarker());
+
+        database.update(DatabaseHelper.markerInfoTable,values,DatabaseHelper.colMarkerInfoID+" = "+markerInfo.getId(),null);
+
+        close();
+    }
+
+
+}
---
+++ app/src/main/java/be/howest/dylandeceulaer/places/MarkerInfo.java
@@ -0,0 +1,62 @@
+package be.howest.dylandeceulaer.places;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by Dylan on 12/04/2015.
+ */
+public class MarkerInfo {
+
+    private long Id;
+    private String titel;
+    private LatLng positie;
+    private String adres;
+    private Data.MARKER markerData;
+
+    public MarkerInfo(String titel, LatLng positie, String adres, Data.MARKER markerData) {
+        this.titel = titel;
+        this.positie = positie;
+        this.adres = adres;
+        this.markerData = markerData;
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public LatLng getPositie() {
+        return positie;
+    }
+
+    public void setPositie(LatLng positie) {
+        this.positie = positie;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public Data.MARKER getMarkerData() {
+        return markerData;
+    }
+
+    public void setMarkerData(Data.MARKER markerData) {
+        this.markerData = markerData;
+    }
+}
